437f00be917e31e49c3699023b822a858a0cb828
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-03-24 15:13:15 -0700

    added layout

--- onboarding/src/App.js
+++ onboarding/src/App.js
@@ -1,16 +1,191 @@
+import React, { useState, useEffect } from "react";
+
+import FriendForm from "./src/form";
+// 🔥 STEP 1- CHECK THE ENDPOINTS IN THE README
+// 🔥 STEP 2- FLESH OUT FriendForm.js
+// 🔥 STEP 3- FLESH THE SCHEMA IN ITS OWN FILE
+// 🔥 STEP 4- IMPORT THE SCHEMA, AXIOS AND YUP
+import schema from "../validation/formSchema";
+import axios from "axios";
+import * as yup from "yup";
 
 
-function App() {
+function Friend({ details }) {
+  if (!details) {
+    return <h3>Working fetching your friend&apos;s details...</h3>
+  }
+
   return (
-    <div className="Container">
+    <div className='friend container'>
+      <h2>{details.username}</h2>
+      <p>Email: {details.email}</p>
+      <p>Role: {details.role}</p>
+      <p>Civil: {details.civil}</p>
+
+      {
+        !!details.hobbies && !!details.hobbies.length &&
+        <div>
+          Hobbies:
+          <ul>
+            {details.hobbies.map((like, idx) => <li key={idx}>{like}</li>)}
+          </ul>
+        </div>
+      }
+    </div>
+  )
+}
+
+export default Friend
+
+
+const initialFormValues = {
+  ///// TEXT INPUTS /////
+  username: "",
+  email: "",
+  ///// DROPDOWN /////
+  role: "",
+  ///// RADIO BUTTONS /////
+  civil: "",
+  ///// CHECKBOXES /////
+  hiking: false,
+  reading: false,
+  coding: false,
+};
+const initialFormErrors = {
+  username: "",
+  email: "",
+  role: "",
+  civil: "",
+};
+const initialFriends = [];
+const initialDisabled = true;
+
+export default function App() {
+  //////////////// STATES ////////////////
+  //////////////// STATES ////////////////
+  //////////////// STATES ////////////////
+  const [friends, setFriends] = useState(initialFriends); // array of friend objects
+  const [formValues, setFormValues] = useState(initialFormValues); // object
+  const [formErrors, setFormErrors] = useState(initialFormErrors); // object
+  const [disabled, setDisabled] = useState(initialDisabled); // boolean
+
+  //////////////// HELPERS ////////////////
+  //////////////// HELPERS ////////////////
+  //////////////// HELPERS ////////////////
+  const getFriends = () => {
+    // 🔥 STEP 5- IMPLEMENT! ON SUCCESS PUT FRIENDS IN STATE
+    //    helper to [GET] all friends from `http://buddies.com/api/friends`
+    axios
+      .get("http://buddies.com/api/friends")
+      .then((res) => {
+        setFriends(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const postNewFriend = (newFriend) => {
+    // 🔥 STEP 6- IMPLEMENT! ON SUCCESS ADD NEWLY CREATED FRIEND TO STATE
+    //    helper to [POST] `newFriend` to `http://buddies.com/api/friends`
+    //    and regardless of success or failure, the form should reset
+    axios
+      .post("http://buddies.com/api/friends", newFriend)
+      .then((res) => {
+        setFriends([res.data, ...friends]);
+        setFormValues(initialFormValues);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  //////////////// EVENT HANDLERS ////////////////
+  //////////////// EVENT HANDLERS ////////////////
+  //////////////// EVENT HANDLERS ////////////////
+  const inputChange = (name, value) => {
+    // 🔥 STEP 10- RUN VALIDATION WITH YUP
+      // yup.reach will allow us to "reach" into the schema and test only one part.
+    // We give reach the schema as the first argument, and the key we want to test as the second.
+    yup
+    .reach(schema, name) // get to this part of the schema
+//we can then run validate using the value
+    .validate(value) // validate this value
+    .then(() => {
+      // happy path and clear the error
+      setFormErrors({
+        ...formErrors,
+        [name]: "",
+      });
+    })
+// if the validation is unsuccessful, we can set the error message to the message
+    // returned from yup (that we created in our schema)
+    .catch((err) => {
+      setFormErrors({
+        ...formErrors,
+        // validation error from schema
+        [name]: err.errors[0],
+      });
+    });
+    
+    setFormValues({
+      ...formValues,
+      [name]: value, // NOT AN ARRAY
+    });
+  };
+
+  const formSubmit = () => {
+    const newFriend = {
+      username: formValues.username.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role.trim(),
+      civil: formValues.civil.trim(),
+      // 🔥 STEP 7- WHAT ABOUT HOBBIES?
+      hobbies: ["coding", "reading", "hiking"].filter(
+        (hobby) => formValues[hobby]
+      ),
+    };
+    // 🔥 STEP 8- POST NEW FRIEND USING HELPER
+    postNewFriend(newFriend);
+  };
+
+  //////////////// SIDE EFFECTS ////////////////
+  //////////////// SIDE EFFECTS ////////////////
+  //////////////// SIDE EFFECTS ////////////////
+  useEffect(() => {
+    getFriends();
+  }, []);
+
+  /* Each time the form value state is updated, check to see if it is valid per our schema. 
+  This will allow us to enable/disable the submit button.*/
+  useEffect(() => {
+    // 🔥 STEP 9- ADJUST THE STATUS OF `disabled` EVERY TIME `formValues` CHANGES
+    /* We pass the entire state into the entire schema, no need to use reach here. 
+    We want to make sure it is all valid before we allow a user to submit
+    isValid comes from Yup directly */
+    schema.isValid(formValues).then((valid) => {
+      setDisabled(!valid);
+    });
+  }, [formValues]);
+
+  return (
+    <div className="container">
       <header>
-        <h1>
-          Lambda OnBoarding
-        </h1>
-   
+        <h1>Friends App</h1>
       </header>
+
+      <FriendForm
+        values={formValues}
+        change={inputChange}
+        submit={formSubmit}
+        disabled={disabled}
+        errors={formErrors}
+      />
+
+      {friends.map((friend) => {
+        return <Friend key={friend.id} details={friend} />;
+      })}
     </div>
   );
 }
 
-export default App;
--- onboarding/src/index.js
+++ onboarding/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// This is for the fake API. Do not delete!
+import { worker } from './api-mocks/browser'
+worker.start()
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 👉 Importing React libs from node_modules folder
+import React from 'react'
+import { render, ReactDOM } from 'react-dom'
+
+// 👉 Importing the top-level component
+import App from './components/App'
+
+render(
+  <App />
+  , document.querySelector('#root')
+)
+
