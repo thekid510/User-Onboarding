00e44d3cf30c335e6c0ebe31450746b1eed7aac7
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-03-25 15:55:17 -0700

    trouble shooting failed tests

--- cypress/integration/examples/form_test.js
+++ cypress/integration/examples/form_test.js
@@ -2,26 +2,51 @@ describe("Form App", ()=>{
     beforeEach(()=>{
         cy.visit("http://localhost:3003/")
     })
-    const nameInput = () => cy.get('input[name="name"]');
+    const nameInput = () => cy.get('input[name="username"]');
     const emailInput = () => cy.get('input[name="email"]')
     const passwordInput = () => cy.get('input[name="password"]')
-    const termsInput = () => cy.get('input[name="tos"]')
+    const termsInput = () => cy.get('input[name="Terms"]')
     const submitButton = () => cy.get('#submitButton')
+
     it("sanity test to make sure that tests work", () => {
     
         expect(1 + 2).to.equal(3);
         expect(2 + 2).not.to.equal(5);
       });
-    it("running input tests", ()=>{
-        nameInput().should("have.value", "");
-        emailInput().should("have.value", "");
-        passwordInput().should("have.value", "");
-        termsInput().should("not.be.checked");
+      it("the proper elements are showing on the screen", () => {
+        nameInput().should("exist");
+        cy.get('input[name="foobar"]').should("not.exist");
+        emailInput().should("exist");
+        passwordInput().should("exist");
+        submitButton().should("exist");
+        cy.contains(/submit/i);
+        expect({}).not.to.equal({}); // not strict (===)
+        expect({}).to.eql({}); // strict
+      });
 
-        nameInput().type("James Freund");
-        nameInput().should("have.value", "James Freund");
-        emailInput().type("jamesf@gmail.com");
-        passwordInput().type("codingisfun");
+    it("running input tests", ()=>{
+        nameInput()
+        .should("have.value", "")
+        .type("James")
+        .should("have.value","James");
+
+        emailInput()
+        .should("have.value", "")
+        .type("jamesf@gmail.com")
+        .should("have.value","jamesf@gmail.com");
+
+        passwordInput()
+        .should("have.value", "")
+        .type("codingisfun")
+        .should("have.value", "codingisfun");
+
+        termsInput()
+        .should("not.be.checked");
+
+        
+        
+        
+       
         submitButton().should("be disabled");
     });
 });
